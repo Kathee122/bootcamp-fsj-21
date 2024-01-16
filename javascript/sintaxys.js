ed5f297aff4e484a76cf4c1e407403daa37fefa1
Kathee122 <kathepaiz04@gmail.com>
2024-01-16 10:56:29 -0600

    subir cambios javascript

---
+++ javascript/sintaxys.js
@@ -0,0 +1,70 @@
+//comentarios = partes de codigo que no se ejecutan
+//comentario de una sola linea 
+
+/* esto es un 
+    comentario
+        multilineas*/
+
+//---- tipo de datos -----
+/*
+numerico = number
+Texto = string
+Caracter = string
+Boleanos = Boolean 
+*/
+
+//---- Variables -----
+// Variable = Un lugar o espacio en el cual podemos guardar algo
+// ESE DATO TIENE QUE SER ALGO QUE CAMBIE
+
+//---- tipo de variables -----
+// Let y var
+//palabra reservada + nombre de la variable + asignacion + valor a guada
+var nombre = "Katherine"
+
+//---- sintaxis tipos de datos ---
+var numerico = 30
+var boleanoverdadero = true
+var boleanofalso = false
+var caracter = "a"
+var texto = 'holiwi soy un texto'
+
+//---- tipo de variables -----
+// Let y var
+// var = palabra reservada para declarar variables
+// let = es la segunda palabra reservada y la mas usada
+//var permite redeclarar y let  no
+
+let edad = "25"
+console.log("holi como andis")
+console.log(edad)
+edad = "26"
+console.log(edad)
+
+// --- operadores matematicos ---
+let suma = 5 + 5
+let resta = 6 - 3
+let division = 138 / 2
+let multiplicacion = 28 * 2
+let residuo = 10 % 2
+
+// --- operadores logicos ---
+// ESTOS DAN COMO RESULTADO UN BOOLEANO
+let igualdad = 10 == 10
+console.log(igualdad)
+let igualdadEstricta = 10 === "10"
+console.log(igualdadEstricta)
+let desigualdad = 10 != 10
+console.log(desigualdad)
+let desigualdadEstricta = 10 !== "10"
+console.log(desigualdadEstricta);
+
+// y = &&
+// siempre va a esperar que ambas cosas sean true
+let y = 10 == 10 && 10 === "10"
+console.log(y)
+// 0 = ||
+//o siempre va a esperar que 1 de las sea true
+let o = 10 == 10 && 10 === "10"
+console.log(0)
+
